b7367cfdf6e6c5acced83ba7a04021bfac8643a5
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-21 00:13:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
 	let newhex="#";
+	if(shortHex.charAt(0)!="#"){
+		shortHex="#"+shortHex;
+	}
+	
+	
 	
 	for(let i=1;i<shortHex.length;i++){
 		newhex=newhex+shortHex.charAt(i)+shortHex.charAt(i)
